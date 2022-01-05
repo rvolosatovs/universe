b8faa106aa8df0a2710f7d0c8bfd2841463d3062
rvolosatovs <rvolosatovs@users.noreply.github.com>
2022-01-05 00:44:29 +0000

    Deploying to gh-pages from @ rvolosatovs/universe@aa7adc49d369f015977555232cd92594eadc302a 🚀

--- rust/sallyport/guest/syscall/sidebar-items.js
+++ rust/sallyport/guest/syscall/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["ERRNO_START",""],["MAX_ERRNO",""]],"struct":[["Argv",""],["Exit",""],["Read",""],["Result",""]]});
+initSidebarItems({"constant":[["ERRNO_START",""],["MAX_ERRNO",""]],"struct":[["Argv",""],["Exit",""],["Read",""],["Result",""],["StagedWrite",""],["Write",""]]});
